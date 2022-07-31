1be48e66ad1526655ffc095fe0d529714ed1a080
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2022-07-31 16:38:44 -0500

    feat: Add code so path & redirect_url are sent to backend and ui changes for that.

---
+++ frontend/index.js
@@ -0,0 +1,31 @@
+let afterSubmit = document.getElementById("after-submit")
+afterSubmit.hidden = true
+let form = document.getElementById('url-form')
+let redirectUrl = document.getElementById("redirect-url")
+let path = document.getElementById("path")
+
+form.onsubmit = async function (event) {
+    event.preventDefault();
+
+    let response = await fetch("http://127.0.0.1:8000/add-url", {
+        method: "post",
+        body: new FormData(form),
+        headers: {"csrf-token": ""}
+    })
+    let json_response = await response.json()
+    if (response.ok === true) {
+        form.hidden = true
+        afterSubmit.hidden = false
+        form.reset()
+        redirectUrl.value = json_response.redirect_url
+        path.value = "url.manavrv.dev/" + json_response.path
+
+    } else if (json_response.detail === "Not a valid url.")  {
+        alert("Please input a valid Url.")
+    } else if (json_response.detail === "Path already in use.") {
+        alert("That alias is already in use. Please try another one.")
+    } else if (response.status === 422) {
+        alert("Please fill out both parts before submitting.")
+    }
+
+}
